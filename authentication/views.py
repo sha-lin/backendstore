c385f712e194bb1dbcbbbcd5550679d008d33b0c
shalin <shalin.rono@student.moringaschool.com>
2021-11-02 15:05:42 +0300

    create login api view

--- authentication/views.py
+++ authentication/views.py
@@ -63,3 +63,11 @@ class VerifyEmail(views.APIView):
             return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
         except jwt.exceptions.DecodeError as identifier:
             return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
