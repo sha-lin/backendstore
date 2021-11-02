2b8e3a592979a58af6c8db626518f3971e1505ff
shalin <shalin.rono@student.moringaschool.com>
2021-11-02 11:23:27 +0300

    create user manager class

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin)
+
+from django.db import models
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Users should have a username')
+        if email is None:
+            raise TypeError('Users should have a Email')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        if password is None:
+            raise TypeError('Password should not be none')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+# AUTH_PROVIDERS = {'facebook': 'facebook', 'google': 'google',
+#                   'twitter': 'twitter', 'email': 'email'}
+
